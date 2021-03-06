f7e6fde64b3a96778670a2334de77dd523eda63b
Cephas king'ori Wangari <cephasmarquis2000@gmail.com>
2021-03-06 23:05:23 +0300

    the homepage

---
+++ js/slide.js
@@ -0,0 +1,14 @@
+$('.featured').slick({
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplayspeed: 1000,
+    centerMode: true,
+  });
+
+
+
+  
